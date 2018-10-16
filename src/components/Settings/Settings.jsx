3cbbc2697e8a7ebc1c77fd8d14e3017394b489c1
jrmccann2 <jshmccann@gmail.com>
2018-10-16 16:49:35 -0600

    added in skeleton components

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+
+        currentEmail: '',
+        newEmail: ''
+
+    }
+  }
+
+  handleChange(){
+
+  }
+
+  updateEmail(){
+
+  }
+
+  deleteAcct(){
+
+  }
+
+  render() {
+    return (
+      <div className="Settings">
+        Settings
+      </div>
+    );
+  }
+}
+
+export default Settings;
---
+++ src/components/TeamDash/TeamDash.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class TeamDash extends Component {
+
+  getTeamStats(){
+
+  }
+
+  getRoster(){
+
+  }
+
+  render() {
+    return (
+      <div className="TeamDash">
+        Register Team
+      </div>
+    );
+  }
+}
+
+export default TeamDash;
